67813e6fdf6b2ebecf9063a8cdb9dec2db2f9d4f
Andrew Clark <mrandyclark@gmail.com>
2008-11-17 12:35:44 -0500

    missing comma in photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,7 +6,7 @@ class Photo < ActiveRecord::Base
   has_attachment :content_type => :image, 
                    :storage => :s3, 
                    :processor => :ImageScience,
-                   :max_size => 1.megabytes
+                   :max_size => 1.megabytes,
                    :thumbnails => { :thumb => '150x150>' }
 
   validates_as_attachment
