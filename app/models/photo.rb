a7a604b66e6b1735c4ffdc225c09f2c4893eacdb
Andrew Clark <mrandyclark@gmail.com>
2008-11-17 12:32:40 -0500

    include image science

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,13 @@
+include ImageScience
+# include Rmagick
+
 class Photo < ActiveRecord::Base
 
   has_attachment :content_type => :image, 
                    :storage => :s3, 
-                   # :processor => :ImageScience,
+                   :processor => :ImageScience,
                    :max_size => 1.megabytes
-                   # :thumbnails => { :thumb => '150x150>' }
+                   :thumbnails => { :thumb => '150x150>' }
 
   validates_as_attachment
 
