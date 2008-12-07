bc9b8e7e731fda74eac03af267752a93601bef03
Andrew Clark <mrandyclark@gmail.com>
2008-12-07 08:51:45 -0500

    broken paperclips, trial 5

--- config/environment.rb
+++ config/environment.rb
@@ -73,8 +73,8 @@ end
 # # These are the sizes of the domain (i.e. 0 for localhost, 1 for something.com)  
 # # for each of your environments  
 # # SubdomainFu.tld_sizes = { :development => 1,  
-                           :test => 0,  
-                           :production => 1 }  
+# #                           :test => 0,  
+# #                           :production => 1 }  
 
 # # These are the subdomains that will be equivalent to no subdomain  
 # # SubdomainFu.mirrors = ["www"]  
