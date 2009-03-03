c2bd5eb95a3f2a364cb0150a54e056fa1412c3ef
Andrew Clark <mrandyclark@gmail.com>
2009-03-03 11:04:20 -0500

    changes deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,47 @@
-# require 'mongrel_cluster/recipes'
-# set :stages, %w(slice slice2)
-# require 'capistrano/ext/multistage'
+#############################################################
+#	Settings
+#############################################################
+default_run_options[:pty] = true
+set :use_sudo, true
 
 set :application, "stylfy"
 
-set :domain, "173.45.226.52"
-
-set :user, "deploy"
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
 set :deploy_to, "/home/andy/apps/#{application}"
 
-set :scm, "git"
-set :deploy_via, :copy
-#set :deploy_via, :remote_cache
-set :copy_cache, true
-set :repository,  "git@github.com:mrandyclark/stylfy.git" 
-set :branch, "master"
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
 
-set :chmod755, %w(app config db lib public vendor script tmp public/dispatch.cgi public/dispatch.fcgi public/dispatch.rb)
-set :use_sudo, false
 default_run_options[:pty] = true
+set :repository,  "git@github.com:mrandyclark/stylfy.git"
+set :scm, "git"
+set :scm_passphrase, "22?15n5" #This is your custom users password
+set :branch, "master"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
 
-## config for ssh
-ssh_options[:port] = 1985
-ssh_options[:username] = 'deploy'
-ssh_options[:host_key] = 'ssh-dss'
+set :user, "andy"
+set :password, "22?15n5"
 
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
+role :app, "173.45.226.52:1985"
+role :web, "173.45.226.52:1985"
+role :db,  "173.45.226.52:1985", :primary => true
+
+#############################################################
+#	Passenger
+#############################################################
 
 namespace :deploy do
-  # reset passenger  
-	desc "Restart Application"
-	task :restart do
-		run "touch #{current_path}/tmp/restart.txt"
-	end
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  desc "Start Application"
+  task :start do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 end
-
-
-
-
